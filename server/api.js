6d6dce065e2e67c79a9476c70b6ca493949a44b7
Konstantinos Papakonstantinou <kpapakonstantinou@idirect.net>
2024-03-01 06:21:09 +0200

    install client app

--- server/api.js
+++ server/api.js
@@ -1,8 +1,6 @@
 const { User } = require('./models')
 const router = require('express').Router()
 
-router.get('/', (req, res) => res.send('Hello from API'))
-
 router.post('/register', async (req, res) => {
   try {
     const user = new User(req.body)
@@ -11,6 +9,102 @@ router.post('/register', async (req, res) => {
   } catch (error) {
     console.error(error)
   }
+})
+
+router.post('/login', async (req, res) => {
+  try {
+    const registeredUser = await User.findOne({ email: req.body.email }).exec()
+    if (registeredUser?.password === req.body.password) {
+      return res.status(200).send(registeredUser)
+    }
+    res.status(401).send('Invalid credentials')
+  } catch (error) {
+    console.error(error)
+  }
+})
+
+router.get('/events', (req, res) => {
+  let events = [
+    {
+      _id: '1',
+      name: 'Auto Expo',
+      description: 'lorem ipsum',
+      date: '2012-04-23T18:25:43.511Z',
+    },
+    {
+      _id: '2',
+      name: 'Auto Expo',
+      description: 'lorem ipsum',
+      date: '2012-04-23T18:25:43.511Z',
+    },
+    {
+      _id: '3',
+      name: 'Auto Expo',
+      description: 'lorem ipsum',
+      date: '2012-04-23T18:25:43.511Z',
+    },
+    {
+      _id: '4',
+      name: 'Auto Expo',
+      description: 'lorem ipsum',
+      date: '2012-04-23T18:25:43.511Z',
+    },
+    {
+      _id: '5',
+      name: 'Auto Expo',
+      description: 'lorem ipsum',
+      date: '2012-04-23T18:25:43.511Z',
+    },
+    {
+      _id: '6',
+      name: 'Auto Expo',
+      description: 'lorem ipsum',
+      date: '2012-04-23T18:25:43.511Z',
+    },
+  ]
+  res.json(events)
+})
+
+router.get('/special', (req, res) => {
+  let specialEvents = [
+    {
+      _id: '1',
+      name: 'Auto Expo Special',
+      description: 'lorem ipsum',
+      date: '2012-04-23T18:25:43.511Z',
+    },
+    {
+      _id: '2',
+      name: 'Auto Expo Special',
+      description: 'lorem ipsum',
+      date: '2012-04-23T18:25:43.511Z',
+    },
+    {
+      _id: '3',
+      name: 'Auto Expo Special',
+      description: 'lorem ipsum',
+      date: '2012-04-23T18:25:43.511Z',
+    },
+    {
+      _id: '4',
+      name: 'Auto Expo Special',
+      description: 'lorem ipsum',
+      date: '2012-04-23T18:25:43.511Z',
+    },
+    {
+      _id: '5',
+      name: 'Auto Expo Special',
+      description: 'lorem ipsum',
+      date: '2012-04-23T18:25:43.511Z',
+    },
+    {
+      _id: '6',
+      name: 'Auto Expo Special',
+      description: 'lorem ipsum',
+      date: '2012-04-23T18:25:43.511Z',
+    },
+  ]
+  res.json(specialEvents)
 })
 
 module.exports = router
--- server/server.js
+++ server/server.js
@@ -2,5 +2,4 @@ const express = require('express')
 const server = express()
 server.use(express.json())
 server.use('/api', require('./api'))
-server.get('/', (req, res) => res.send('Hello from server'))
 module.exports = server

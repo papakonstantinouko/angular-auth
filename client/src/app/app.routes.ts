29e32795965c61a262817b011ffba8bc8eeac227
Konstantinos Papakonstantinou <kpapakonstantinou@idirect.net>
2024-03-01 06:30:27 +0200

    install routing

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,3 +1,13 @@
-import { Routes } from '@angular/router';
+import { Routes } from '@angular/router'
+import { RegisterComponent } from './register/register.component'
+import { LoginComponent } from './login/login.component'
+import { EventsComponent } from './events/events.component'
+import { SpecialEventsComponent } from './special-events/special-events.component'
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'register', component: RegisterComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'events', component: EventsComponent },
+  { path: 'special-events', component: SpecialEventsComponent },
+  { path: '', redirectTo: 'events', pathMatch: 'full' },
+]
---
+++ client/src/app/special-events/special-events.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-special-events',
+  standalone: true,
+  imports: [],
+  template: `
+    <p>
+      special-events works!
+    </p>
+  `,
+  styles: ``
+})
+export class SpecialEventsComponent {
+
+}
